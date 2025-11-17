0223cfdf18ae4d0ae6b00ea565ab02fe6abab64f
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 05:35:38 -0300

    Atualizar o page.js

--- app/register/page.js
+++ app/register/page.js
@@ -1,26 +1,21 @@
 "use client";
 
 import Link from "next/link";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import Header from "../components/Header";
 
 export default function RegisterPage() {
   return (
     <main style={{ maxWidth: "500px", margin: "0 auto" }}>
+      <Header />
+
       <h1>Criar Conta</h1>
 
       <form style={{ marginTop: "20px" }}>
-        <div style={{ marginBottom: "10px" }}>
-          <label>E-mail</label>
-          <input type="email" placeholder="seu@exemplo.com" style={{ width: "100%" }} />
-        </div>
-
-        <div style={{ marginBottom: "10px" }}>
-          <label>Senha</label>
-          <input type="password" placeholder="••••••••" style={{ width: "100%" }} />
-        </div>
-
-        <button style={{ background: "#000", color: "#fff", width: "100%" }}>
-          Registrar
-        </button>
+        <Input label="E-mail" type="email" placeholder="seu@exemplo.com" />
+        <Input label="Senha" type="password" placeholder="••••••••" />
+        <Button>Registrar</Button>
       </form>
 
       <div style={{ marginTop: "20px" }}>
