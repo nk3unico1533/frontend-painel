214ea32792585f97a2ad0851271cba1dc1904342
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 05:36:03 -0300

    Atualizar o page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,8 +1,12 @@
+import Header from "../components/Header";
+
 export default function DashboardPage() {
   return (
     <main style={{ padding: "20px" }}>
+      <Header />
+
       <h1>Dashboard</h1>
-      <p>Aqui futuramente entram os dados reais.</p>
+      <p>Aqui futuramente entrarão os dados reais.</p>
     </main>
   );
 }
