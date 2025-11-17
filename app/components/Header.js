1a44a0f2917e33daed8c76cffaef62c712fae4e8
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 06:52:06 -0300

    Atualizar o Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,19 +3,20 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header style={{ display:"flex", justifyContent:"space-between", alignItems:"center", marginBottom:12 }}>
-      <div className="brand">
-        <div className="logo">PN</div>
+    <header className="header">
+      <div className="header-top">
+        <div className="logo-circle">DA</div>
+
         <div>
-          <div style={{color:"white", fontWeight:700}}>Painel Consulta</div>
-          <div className="small">Dark Neon • Vaporwave</div>
+          <div className="header-title">Dark Aurora Painel</div>
+          <div className="header-sub">By nk</div>
         </div>
       </div>
 
-      <nav style={{display:"flex", gap:12, alignItems:"center"}}>
-        <Link className="btn" href="/">Login</Link>
-        <Link className="btn" href="/register">Registrar</Link>
-        <Link className="btn" href="/dashboard">Dashboard</Link>
+      <nav className="nav">
+        <Link href="/">Login</Link>
+        <Link href="/register">Registrar</Link>
+        <Link href="/dashboard">Dashboard</Link>
       </nav>
     </header>
   );
