549550d867a1d8c21a2ef71a32a847af9b24ff2f
nk3unico1533 <nk3unico@outlook.com>
2025-11-17 05:35:12 -0300

    Atualizar o page.js

--- app/page.js
+++ app/page.js
@@ -1,30 +1,21 @@
 "use client";
 
 import Link from "next/link";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 export default function LoginPage() {
   return (
     <main style={{ maxWidth: "500px", margin: "0 auto" }}>
-      <h1>Painel • Dark Neon</h1>
+      <Header />
 
-      <div style={{ margin: "20px 0" }}>
-        <Link href="/">Login</Link> | <Link href="/register">Registrar</Link>
-      </div>
-
-      <p>Use seu e-mail e senha para acessar o painel.</p>
+      <h1>Painel • Login</h1>
 
       <form style={{ marginTop: "20px" }}>
-        <div style={{ marginBottom: "10px" }}>
-          <label>E-mail</label>
-          <input type="email" placeholder="seu@exemplo.com" style={{ width: "100%" }} />
-        </div>
-
-        <div style={{ marginBottom: "10px" }}>
-          <label>Senha</label>
-          <input type="password" placeholder="••••••••" style={{ width: "100%" }} />
-        </div>
-
-        <button style={{ background: "#000", color: "#fff", width: "100%" }}>Entrar</button>
+        <Input label="E-mail" type="email" placeholder="seu@exemplo.com" />
+        <Input label="Senha" type="password" placeholder="••••••••" />
+        <Button>Entrar</Button>
       </form>
 
       <div style={{ marginTop: "20px" }}>
